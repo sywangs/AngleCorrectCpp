50eddd7e4d11d50fe1e6e69bd81bcb51f3724f61
developer <developer@developers-MacBook-Pro.local>
2018-08-08 11:30:03 +0800

    CPP version

---
+++ cmake-build-debug/CMakeFiles/Test2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test2.dir/main.cpp.o"
+  "Test2.pdb"
+  "Test2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
